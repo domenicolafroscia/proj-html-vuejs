0099e3204105bf1bdda995ddd9a38f3769b170e1
domenicolafroscia <domenicolafroscia@gmail.com>
2023-12-04 16:14:07 +0100

    add: header completed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <script>
-export default {
+import AppHeader from './components/AppHeader.vue';
 
-}
+export default {
+  components: { AppHeader },
+  data() {
+    return {
+      itemsNav: [
+      'Home',
+      'Pages',
+      'Courses',
+      'Features',
+      'Blog',
+      'Shop',
+      ]
+    };
+  },
+
+};
 </script>
 
 <template>
-  
+  <AppHeader :items="itemsNav"/>
 </template>
 
 <style lang="scss">
 @use './style/general.scss';
-
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,100 @@
+<script>
+export default {
+    props: {
+        items: Array
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="container d-flex justify-content-between align-items-center">
+            <img class="logo" src="../assets/img/dark-logo.png" alt="">
+            <nav>
+                <ul>
+                    <li v-for="item in items"><a class="link-black" href="">{{ item }} <i class="chevron fa-solid fa-chevron-down" style="color: #0b0b0b;"></i> </a></li>
+                    <li><a href=""><i class="fa-regular fa-circle-user" style="color: #0b0b0b;"></i></a></li>
+                </ul>
+                <div class="search">
+                    <input class="search-text" type="text" placeholder="Search..." id="site-search" name="" />
+                    <button class="search-btn"><i class="fa-solid fa-magnifying-glass" style="color: #1fad96;"></i></button>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped lang="scss"> 
+@use "../style/partials/variables" as *;
+@import "@fortawesome/fontawesome-free/css/all.min.css";
+
+
+header {
+    border-bottom: 4px solid $hint-of-red;
+    z-index: 200;
+    position: fixed;
+    width: 100%;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .logo {
+            width: 200px;
+        }
+
+        nav {
+            display: flex;
+
+            ul {
+                display: flex;
+                margin: 2rem;
+
+                li {
+                    padding: 0 .5rem;
+
+                    .link-black {
+                        color: $cod-gray;
+                        display: flex;
+                        padding: 0 20px;
+                        gap: .3rem;
+                        align-items: center;
+
+                        .chevron {
+                            font-size: 10px;
+                        }
+                    }
+                }
+            }
+        
+
+            .search {
+                position: relative;
+
+                .search-text {
+                    height: 40px;
+                    background-color: $hint-of-red;
+                    border: none;
+                    margin-left: 1rem;
+                    border-radius: 5px;
+                    margin-top: 1rem;
+
+                }
+
+                .search-btn {
+                    border: none;
+                    background: none;
+                    position: absolute;
+                    right: 10px;
+                    top: 28px;
+                    cursor: pointer;
+                }
+            }
+        }
+        
+    }
+}
+
+ 
+</style>
